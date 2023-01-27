b82320d4506d126f8612f0ea87c55ce2ffad3404
Mike Spreitzer <mspreitz@us.ibm.com>
2023-01-27 00:28:19 -0500

    Finish first draft of mailbox controller

--- cmd/mailbox-controller/main.go
+++ cmd/mailbox-controller/main.go
@@ -21,6 +21,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/spf13/pflag"
@@ -30,20 +31,38 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/workqueue"
 	"k8s.io/klog/v2"
 
 	apisv1alpha1 "github.com/kcp-dev/kcp/pkg/apis/apis/v1alpha1"
+	tenancyv1alpha1 "github.com/kcp-dev/kcp/pkg/apis/tenancy/v1alpha1"
 	"github.com/kcp-dev/kcp/pkg/apis/third_party/conditions/util/conditions"
+	workloadv1alpha1 "github.com/kcp-dev/kcp/pkg/apis/workload/v1alpha1"
 	kcpclientset "github.com/kcp-dev/kcp/pkg/client/clientset/versioned"
 	kcpclusterclientset "github.com/kcp-dev/kcp/pkg/client/clientset/versioned/cluster"
+	tenancyclient "github.com/kcp-dev/kcp/pkg/client/clientset/versioned/typed/tenancy/v1alpha1"
 	kcpinformers "github.com/kcp-dev/kcp/pkg/client/informers/externalversions"
+	tenancylisters "github.com/kcp-dev/kcp/pkg/client/listers/tenancy/v1alpha1"
+	workloadlisters "github.com/kcp-dev/kcp/pkg/client/listers/workload/v1alpha1"
+	"github.com/kcp-dev/logicalcluster/v3"
 )
+
+type mbCtl struct {
+	context          context.Context
+	synctargetLister workloadlisters.SyncTargetClusterLister
+	workspaceLister  tenancylisters.WorkspaceLister
+	workspaceClient  tenancyclient.WorkspaceInterface
+	queue            workqueue.RateLimitingInterface
+}
 
 func main() {
 	resyncPeriod := time.Duration(0)
+	var concurrency int = 4
 	fs := pflag.NewFlagSet("mailbox-controller", pflag.ExitOnError)
 	klog.InitFlags(flag.CommandLine)
 	fs.AddGoFlagSet(flag.CommandLine)
+
+	fs.IntVar(&concurrency, "concurrency", concurrency, "number of syncs to run in parallel")
 
 	inventoryLoadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
 	inventoryConfigOverrides := &clientcmd.ConfigOverrides{}
@@ -60,7 +79,8 @@ func main() {
 	fs.Parse(os.Args[1:])
 
 	ctx := context.Background()
-	logger := klog.FromContext(ctx)
+	logger := klog.Background()
+	ctx = klog.NewContext(ctx, logger)
 
 	// create config for accessing TMC service provider workspace
 	inventoryClientConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(inventoryLoadingRules, inventoryConfigOverrides)
@@ -86,7 +106,8 @@ func main() {
 	}
 
 	tmcInformerFactory := kcpinformers.NewSharedInformerFactoryWithOptions(tmcClusterClientset, resyncPeriod)
-	syncTargetsInformer := tmcInformerFactory.Workload().V1alpha1().SyncTargets().Informer()
+	syncTargetsPreInformer := tmcInformerFactory.Workload().V1alpha1().SyncTargets()
+	syncTargetsInformer := syncTargetsPreInformer.Informer()
 
 	// create config for accessing TMC service provider workspace
 	workspacesClientConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(workloadLoadingRules, workloadConfigOverrides)
@@ -98,22 +119,38 @@ func main() {
 
 	workspacesConfig.UserAgent = "mailbox-controller"
 
-	workspacesClient, err := kcpclientset.NewForConfig(workspacesConfig)
+	workspacesClientset, err := kcpclientset.NewForConfig(workspacesConfig)
 	if err != nil {
 		logger.Error(err, "Failed to create clientset for workspaces")
 	}
 
-	workspacesInformerFactory := kcpinformers.NewSharedScopedInformerFactoryWithOptions(workspacesClient, resyncPeriod)
-	workspacesInformer := workspacesInformerFactory.Tenancy().V1alpha1().Workspaces().Informer()
+	workspacesInformerFactory := kcpinformers.NewSharedScopedInformerFactoryWithOptions(workspacesClientset, resyncPeriod)
+	workspacesPreInformer := workspacesInformerFactory.Tenancy().V1alpha1().Workspaces()
+	workspacesInformer := workspacesPreInformer.Informer()
+
+	ctl := &mbCtl{
+		context:          ctx,
+		synctargetLister: syncTargetsPreInformer.Lister(),
+		workspaceLister:  workspacesPreInformer.Lister(),
+		workspaceClient:  workspacesClientset.TenancyV1alpha1().Workspaces(),
+		queue:            workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "mailbox-controller"),
+	}
 
 	onAdd := func(obj any) {
-		logger.Info("Observed add", "obj", obj)
+		logger.V(4).Info("Observed add", "obj", obj)
+		ctl.enqueue(obj)
 	}
 	onUpdate := func(oldObj, newObj any) {
-		logger.Info("Observed update", "oldObj", oldObj, "newObj", newObj)
+		logger.V(4).Info("Observed update", "oldObj", oldObj, "newObj", newObj)
+		if newObj != nil {
+			ctl.enqueue(newObj)
+		} else if oldObj != nil {
+			ctl.enqueue(oldObj)
+		}
 	}
 	onDelete := func(obj any) {
-		logger.Info("Observed delete", "obj", obj)
+		logger.V(4).Info("Observed delete", "obj", obj)
+		ctl.enqueue(obj)
 	}
 	syncTargetsInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc:    onAdd,
@@ -125,9 +162,18 @@ func main() {
 		UpdateFunc: onUpdate,
 		DeleteFunc: onDelete,
 	})
-	tmcInformerFactory.Start(ctx.Done())
-	workspacesInformerFactory.Start(ctx.Done())
-	<-ctx.Done()
+	doneCh := ctx.Done()
+	tmcInformerFactory.Start(doneCh)
+	workspacesInformerFactory.Start(doneCh)
+	if cache.WaitForNamedCacheSync("mailbox-controller", doneCh, syncTargetsInformer.HasSynced, workspacesInformer.HasSynced) {
+		logger.V(1).Info("Informers synced")
+		for worker := 0; worker < concurrency; worker++ {
+			go ctl.syncLoop(ctx, worker)
+		}
+	} else {
+		logger.Error(nil, "Informer syncs not achieved")
+	}
+	<-doneCh
 	logger.Info("Time to stop")
 }
 
@@ -143,7 +189,7 @@ func configForViewOfExport(ctx context.Context, providerConfig *rest.Config, exp
 		apiExport, err = apiExportClient.Get(ctx, exportName, metav1.GetOptions{})
 		if err != nil {
 			if k8sapierrors.IsNotFound(err) {
-				logger.Info("Pause because APIExport not found", "exportName", exportName)
+				logger.V(2).Info("Pause because APIExport not found", "exportName", exportName)
 				time.Sleep(time.Second * 15)
 				continue
 			}
@@ -152,24 +198,144 @@ func configForViewOfExport(ctx context.Context, providerConfig *rest.Config, exp
 		if isAPIExportReady(logger, apiExport) {
 			break
 		}
-		logger.Info("Pause because APIExport not ready", "exportName", exportName)
+		logger.V(2).Info("Pause because APIExport not ready", "exportName", exportName)
 		time.Sleep(time.Second * 15)
 	}
 	viewConfig := rest.CopyConfig(providerConfig)
 	serverURL := apiExport.Status.VirtualWorkspaces[0].URL
-	logger.Info("Found APIExport view", "exportName", exportName, "serverURL", serverURL)
+	logger.V(2).Info("Found APIExport view", "exportName", exportName, "serverURL", serverURL)
 	viewConfig.Host = serverURL
 	return viewConfig, nil
 }
 
 func isAPIExportReady(logger klog.Logger, apiExport *apisv1alpha1.APIExport) bool {
 	if !conditions.IsTrue(apiExport, apisv1alpha1.APIExportVirtualWorkspaceURLsReady) {
-		logger.Info("APIExport virtual workspace URLs are not ready", "APIExport", apiExport.Name)
+		logger.V(2).Info("APIExport virtual workspace URLs are not ready", "APIExport", apiExport.Name)
 		return false
 	}
 	if len(apiExport.Status.VirtualWorkspaces) == 0 {
-		logger.Info("APIExport does not have any virtual workspace URLs", "APIExport", apiExport.Name)
+		logger.V(2).Info("APIExport does not have any virtual workspace URLs", "APIExport", apiExport.Name)
 		return false
 	}
 	return true
+}
+
+func (ctl *mbCtl) enqueue(obj any) {
+	logger := klog.FromContext(ctl.context)
+	switch typed := obj.(type) {
+	case *tenancyv1alpha1.Workspace:
+		logger.V(4).Info("Enqueuing reference due to workspace", "reference", typed.Name)
+		ctl.queue.Add(typed.Name)
+	case *workloadv1alpha1.SyncTarget:
+		cluster := logicalcluster.From(typed)
+		ns := typed.Namespace
+		name := typed.Name
+		logger.V(4).Info("Enqueuing reference due to SyncTarget", "cluster", cluster, "ns", ns, "name", name)
+		ctl.queue.Add(wsNameForTarget(cluster, name))
+	default:
+		logger.Error(nil, "Notified of object of unexpected type", "object", obj, "type", fmt.Sprintf("%T", obj))
+	}
+}
+
+const wsNameSep = "-w-"
+
+func wsNameForTarget(cluster logicalcluster.Name, name string) string {
+	return cluster.String() + wsNameSep + name
+}
+
+func (ctl *mbCtl) syncLoop(ctx context.Context, worker int) {
+	doneCh := ctx.Done()
+	logger := klog.FromContext(ctx)
+	logger = logger.WithValues("worker", worker)
+	ctx = klog.NewContext(ctx, logger)
+	logger.V(4).Info("SyncLoop start")
+	for {
+		select {
+		case <-doneCh:
+			logger.V(2).Info("SyncLoop done")
+			return
+		default:
+			ref, shutdown := ctl.queue.Get()
+			if shutdown {
+				logger.V(2).Info("Queue shutdown")
+				return
+			}
+			ctl.sync1(ctx, ref)
+		}
+	}
+}
+
+func (ctl *mbCtl) sync1(ctx context.Context, ref any) {
+	defer ctl.queue.Done(ref)
+	logger := klog.FromContext(ctx)
+	logger.V(4).Info("Dequeued reference", "ref", ref)
+	retry := ctl.sync(ctx, ref)
+	if retry {
+		ctl.queue.AddRateLimited(ref)
+	} else {
+		ctl.queue.Forget(ref)
+	}
+}
+
+func (ctl *mbCtl) sync(ctx context.Context, ref any) bool {
+	logger := klog.FromContext(ctx)
+	wsName, ok := ref.(string)
+	if !ok {
+		logger.Error(nil, "Sync expected a string", "ref", ref, "type", fmt.Sprintf("%T", ref))
+		return false
+	}
+	wsNameParts := strings.Split(wsName, wsNameSep)
+	if len(wsNameParts) != 2 {
+		logger.Error(nil, "Sync expected a string with 2 parts", "ref", ref, "wsNameParts", wsNameParts)
+		return false
+	}
+	cluster := logicalcluster.Name(wsNameParts[0])
+	targetName := wsNameParts[1]
+	syncTarget, err := ctl.synctargetLister.Cluster(cluster).Get(targetName)
+	if err != nil && !k8sapierrors.IsNotFound(err) {
+		logger.Error(err, "Unable to Get referenced SyncTarget", "wsNameParts", wsNameParts)
+		return true
+	}
+	workspace, err := ctl.workspaceLister.Get(wsName)
+	if err != nil && !k8sapierrors.IsNotFound(err) {
+		logger.Error(err, "Unable to Get referenced Workspace", "wsName", wsName)
+		return true
+	}
+	if syncTarget == nil || syncTarget.DeletionTimestamp != nil {
+		if workspace == nil || workspace.DeletionTimestamp != nil {
+			logger.V(3).Info("Both absent or deleting, nothing to do", "wsName", wsName)
+			return false
+		}
+		err := ctl.workspaceClient.Delete(ctx, wsName, metav1.DeleteOptions{Preconditions: &metav1.Preconditions{UID: &workspace.UID}})
+		if err == nil || k8sapierrors.IsNotFound(err) {
+			logger.V(2).Info("Deleted unwanted workspace", "wsName", wsName)
+			return false
+		}
+		logger.Error(err, "Failed to delete unwanted workspace", "wsName", wsName)
+		return true
+	}
+	// Now we have established that the SyncTarget exists and is not being deleted
+	if workspace == nil {
+		ws := &tenancyv1alpha1.Workspace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: wsName,
+			},
+			Spec: tenancyv1alpha1.WorkspaceSpec{},
+		}
+		_, err := ctl.workspaceClient.Create(ctx, ws, metav1.CreateOptions{FieldManager: "mailbox-controller"})
+		if err != nil {
+			logger.Error(err, "Failed to create workspace", "wsName", wsName)
+			return true
+		}
+		logger.V(2).Info("Created missing workspace", "wsName", wsName)
+		return false
+
+	}
+	if workspace.DeletionTimestamp != nil {
+		logger.V(3).Info("Wanted workspace is being deleted, will retry later", "wsName", wsName)
+		return true
+	}
+	logger.V(3).Info("Both exist and are not being deleted, nothing to do", "wsName", wsName)
+	return false
+
 }
--- docs/poc2023q1/mailbox-controller.md
+++ docs/poc2023q1/mailbox-controller.md
@@ -1,5 +1,20 @@
 # Mailbox Controller
 
+The mailbox controller runs in the edge service provider workspace and
+maintains a child workspace per SyncTarget.
+
+## Usage
+
+The command line flags, beyond the basics, are as follows.
+
+```
+      --concurrency int                  number of syncs to run in parallel (default 4)
+      --inventory-context string         current-context override for inventory-kubeconfig (default "root")
+      --inventory-kubeconfig string      pathname of kubeconfig file for inventory service provider workspace
+      --workload-context string          current-context override for workload-kubeconfig
+      --workload-kubeconfig string       pathname of kubeconfig file for edge workload service provider workspace
+```
+
 ## Try It
 
 To exercise it, do the following steps.
@@ -13,11 +28,50 @@ Start a kcp server.  Do the remaining steps in a separate shell, with
 
 After that, a run of the controller should look like the following.
 
-```
-(base) mspreitz@mjs12 edge-mc % go run ./cmd/mailbox-controller
-I0126 17:29:18.326193   44934 main.go:160] "Found APIExport view" exportName="workload.kcp.io" serverURL="https://192.168.58.123:6443/services/apiexport/root/workload.kcp.io"
+```shell
+(base) mspreitz@mjs12 edge-mc % go run ./cmd/mailbox-controller -v=2
+I0127 00:21:48.876022   24503 main.go:206] "Found APIExport view" exportName="workload.kcp.io" serverURL="https://192.168.58.123:6443/services/apiexport/root/workload.kcp.io"
+I0127 00:21:48.877965   24503 shared_informer.go:255] Waiting for caches to sync for mailbox-controller
+I0127 00:21:48.978352   24503 shared_informer.go:262] Caches are synced for mailbox-controller
+I0127 00:21:48.978414   24503 main.go:169] "Informers synced"
 ```
 
-In a separate shell, create a workspace that is a child of
-`root:edge`.  You should see the controller log the creation and
-several updates of that workspace.
+In a separate shell, `kubectl create` the following object.
+
+```yaml
+apiVersion: workload.kcp.io/v1alpha1
+kind: SyncTarget
+metadata:
+  name: stest1
+spec:
+  cells:
+    foo: bar
+```
+
+That should provoke logging like the following from the mailbox controller.
+
+```
+I0127 00:23:52.545266   24503 main.go:330] "Created missing workspace" worker=1 wsName="269p7excqtb3xen8-w-stest1"
+```
+
+And you can verify that like so.
+
+```shell
+(base) mspreitz@mjs12 kcp % kubectl get Workspace
+NAME                        TYPE        PHASE   URL                                                     AGE
+269p7excqtb3xen8-w-stest1   universal           https://192.168.58.123:6443/clusters/2ktljajura89bwf2   29s
+```
+
+Next, `kubectl delete` that Workspace, and watch the mailbox
+controller wait for it to be gone and then re-create it.
+
+```
+I0127 00:26:40.251001   24503 main.go:330] "Created missing workspace" worker=2 wsName="269p7excqtb3xen8-w-stest1"
+```
+
+Finally, `kubectl delete SyncTarget stest1` and watch the mailbox
+controller react as follows.
+
+```
+I0127 00:26:59.369990   24503 main.go:311] "Deleted unwanted workspace" worker=1 wsName="269p7excqtb3xen8-w-stest1"
+```
