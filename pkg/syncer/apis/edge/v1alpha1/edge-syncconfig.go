6693515a34678162dd3adafb750dca810b1916c2
Takumi Yanagawa <yana@jp.ibm.com>
2023-03-20 20:22:46 +0900

    Add syncconfig api and code generatio

---
+++ pkg/syncer/apis/edge/v1alpha1/edge-syncconfig.go
@@ -0,0 +1,67 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +crd
+// +genclient
+// +genclient:nonNamespaced
+// +kubebuilder:resource:scope=Cluster,shortName=esc
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type EdgeSyncConfig struct {
+	/*
+	 */
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   EdgeSyncConfigSpec   `json:"spec,omitempty"`
+	Status EdgeSyncConfigStatus `json:"status,omitempty"`
+}
+
+// EdgeSyncConfigSpec defines the desired state of EdgeSyncConfig
+type EdgeSyncConfigSpec struct {
+	// Downsncyed resource list
+	DownSyncedResources []EdgeSyncConfigResource `json:"downSyncedResources,omitempty"`
+
+	// Upsncyed resource list
+	UpSyncedResources []EdgeSyncConfigResource `json:"upSyncedResources,omitempty"`
+}
+
+// Resource specifies down/up synced resource with exact GVK and name (and namespace if not cluster scoped resource)
+type EdgeSyncConfigResource struct {
+	// Kind of down/up synced resource
+	Kind string `json:"kind,omitempty"`
+
+	// Group of down/up synced resource
+	Group string `json:"group,omitempty"`
+
+	// Version of down/up synced resource
+	Version string `json:"version,omitempty"`
+
+	// Name of down/up synced resource
+	Name string `json:"name,omitempty"`
+
+	// Namespace of down/up synced resource if it's not cluster scoped resource
+	// +optional
+	Namespace string `json:"namespace,omitempty"`
+}
+
+// EdgeSyncConfigStatus defines the observed state of EdgeSyncConfig
+type EdgeSyncConfigStatus struct {
+	// A timestamp indicating when the syncer last reported status.
+	// +optional
+	LastSyncerHeartbeatTime *metav1.Time `json:"lastSyncerHeartbeatTime,omitempty"`
+}
+
+// EdgePlacementList is the API type for a list of EdgePlacement
+//
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type EdgeSyncConfigList struct {
+	metav1.TypeMeta `json:",inline"`
+	// Standard list metadata.
+	// More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds
+	// +optional
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []EdgeSyncConfig `json:"items"`
+}
---
+++ pkg/syncer/scripts/update-codegen-clients.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+# Copyright 2021 The KCP Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+export GOPATH=$(go env GOPATH)
+
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/../../..
+pushd "${SCRIPT_ROOT}"
+BOILERPLATE_HEADER="$( pwd )/hack/boilerplate/boilerplate.go.txt"
+popd
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; go list -f '{{.Dir}}' -m k8s.io/code-generator)}
+
+# "core:v1alpha1 workload:v1alpha1 apiresource:v1alpha1 tenancy:v1alpha1 tenancy:v1beta1 apis:v1alpha1 scheduling:v1alpha1 topology:v1alpha1" \
+bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client" \
+  github.com/kcp-dev/edge-mc/pkg/syncer/client github.com/kcp-dev/edge-mc/pkg/syncer/apis \
+  "edge:v1alpha1" \
+  --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate/boilerplate.generatego.txt \
+  --output-base "${SCRIPT_ROOT}" \
+  --trim-path-prefix github.com/kcp-dev/edge-mc
+
+pushd ./pkg/syncer/apis
+${CODE_GENERATOR} \
+  "client:outputPackagePath=github.com/kcp-dev/edge-mc/pkg/syncer/client,apiPackagePath=github.com/kcp-dev/edge-mc/pkg/syncer/apis,singleClusterClientPackagePath=github.com/kcp-dev/edge-mc/pkg/syncer/client/clientset/versioned,headerFile=${BOILERPLATE_HEADER}" \
+  "lister:apiPackagePath=github.com/kcp-dev/edge-mc/pkg/syncer/apis,headerFile=${BOILERPLATE_HEADER}" \
+  "informer:outputPackagePath=github.com/kcp-dev/edge-mc/pkg/syncer/client,apiPackagePath=github.com/kcp-dev/edge-mc/pkg/syncer/apis,singleClusterClientPackagePath=github.com/kcp-dev/edge-mc/pkg/syncer/client/clientset/versioned,headerFile=${BOILERPLATE_HEADER}" \
+  "paths=./..." \
+  "output:dir=./../client"
+popd
+
+# bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy" \
+#   github.com/kcp-dev/edge-mc/third_party/conditions/client github.com/kcp-dev/edge-mc/third_party/conditions/apis \
+#   "conditions:v1alpha1" \
+#   --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate/boilerplate.generatego.txt \
+#   --output-base "${SCRIPT_ROOT}" \
+#   --trim-path-prefix github.com/kcp-dev/edge-mc
+
+# bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client" \
+#   github.com/kcp-dev/edge-mc/test/e2e/fixtures/wildwest/client github.com/kcp-dev/edge-mc/test/e2e/fixtures/wildwest/apis \
+#   "wildwest:v1alpha1" \
+#   --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate/boilerplate.generatego.txt \
+#   --output-base "${SCRIPT_ROOT}" \
+#   --trim-path-prefix github.com/kcp-dev/edge-mc
+
+# pushd ./test/e2e/fixtures/wildwest/apis
+# ${CODE_GENERATOR} \
+#   "client:outputPackagePath=github.com/kcp-dev/edge-mc/test/e2e/fixtures/wildwest/client,apiPackagePath=github.com/kcp-dev/edge-mc/test/e2e/fixtures/wildwest/apis,singleClusterClientPackagePath=github.com/kcp-dev/kcp/test/e2e/fixtures/wildwest/client/clientset/versioned,headerFile=${BOILERPLATE_HEADER}" \
+#   "lister:apiPackagePath=github.com/kcp-dev/edge-mc/test/e2e/fixtures/wildwest/apis,headerFile=${BOILERPLATE_HEADER}" \
+#   "informer:outputPackagePath=github.com/kcp-dev/edge-mc/test/e2e/fixtures/wildwest/client,singleClusterClientPackagePath=github.com/kcp-dev/kcp/test/e2e/fixtures/wildwest/client/clientset/versioned,apiPackagePath=github.com/kcp-dev/edge-mc/test/e2e/fixtures/wildwest/apis,headerFile=${BOILERPLATE_HEADER}" \
+#   "paths=./..." \
+#   "output:dir=./../client"
+# popd
+
+go install "${CODEGEN_PKG}"/cmd/openapi-gen
+
+# "$GOPATH"/bin/openapi-gen  \
+# # --input-dirs github.com/kcp-dev/edge-mc/pkg/apis/workload/v1alpha1 \
+# # --input-dirs github.com/kcp-dev/edge-mc/pkg/apis/core/v1alpha1 \
+# # --input-dirs github.com/kcp-dev/edge-mc/pkg/apis/apiresource/v1alpha1 \
+# # --input-dirs github.com/kcp-dev/edge-mc/pkg/apis/tenancy/v1alpha1 \
+# # --input-dirs github.com/kcp-dev/edge-mc/pkg/apis/tenancy/v1beta1 \
+# # --input-dirs github.com/kcp-dev/edge-mc/pkg/apis/apis/v1alpha1 \
+# # --input-dirs github.com/kcp-dev/edge-mc/pkg/apis/scheduling/v1alpha1 \
+# # --input-dirs github.com/kcp-dev/edge-mc/pkg/apis/topology/v1alpha1 \
+# # --input-dirs github.com/kcp-dev/edge-mc/pkg/apis/third_party/conditions/apis/conditions/v1alpha1 \
+# --input-dirs k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/version \
+# --output-package github.com/kcp-dev/edge-mc/pkg/openapi -O zz_generated.openapi \
+# --go-header-file ./hack/../hack/boilerplate/boilerplate.generatego.txt \
+# --output-base "${SCRIPT_ROOT}" \
+# --trim-path-prefix github.com/kcp-dev/edge-mc
