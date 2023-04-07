11c858cfb813b11ed6ac13920cf7f0228ca26b3f
Mike Spreitzer <mspreitz@us.ibm.com>
2023-04-07 02:26:03 -0400

    Define edge syncer config based on EdgePlacement semantics
    Signed-off-by: Mike Spreitzer <mspreitz@us.ibm.com>

--- pkg/apis/edge/v1alpha1/register.go
+++ pkg/apis/edge/v1alpha1/register.go
@@ -51,6 +51,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&SinglePlacementSliceList{},
 		&Customizer{},
 		&CustomizerList{},
+		&SyncerConfig{},
+		&SyncerConfigList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
