10d839be1fa714c3428ca94d95bf91bb7442165a
FRANCO STELLARI <50019234+francostellari@users.noreply.github.com>
2023-05-03 19:51:49 -0400

    Update install-kcp-with-plugins.sh

--- hack/install-kcp-with-plugins.sh
+++ hack/install-kcp-with-plugins.sh
@@ -45,55 +45,55 @@ get_os_type() {
 
 get_arch_type() {
   case "$HOSTTYPE" in
-      x86_64*)	echo "amd64" ;;
+      x86_64*)    echo "amd64" ;;
       aarch64*) echo "arm64" ;;
       *)        echo "Unsupported architecture type: $HOSTTYPE" >&2 ; exit 1 ;;
   esac
 }
 
 get_latest_version() {
-	curl -sL https://github.com/kcp-dev/kcp/releases/latest | grep "</h1>" | head -n 1 | sed -e 's/<[^>]*>//g' | xargs
+    curl -sL https://github.com/kcp-dev/kcp/releases/latest | grep "</h1>" | head -n 1 | sed -e 's/<[^>]*>//g' | xargs
 }
 
 get_full_path() {
-	echo "$(cd "$1"; pwd)"
+    echo "$(cd "$1"; pwd)"
 }
 
 while (( $# > 0 )); do
     case "$1" in
-	(--version)
-	    if (( $# > 1 ));
-	    then { kcp_version="$2"; shift; }
-	    else { echo "$0: missing release version" >&2; exit 1; }
-	    fi;;
-	(--os)
-	    if (( $# > 1 ));
-	    then { kcp_os="$2"; shift; }
-	    else { echo "$0: missing OS type" >&2; exit 1; }
-	    fi;;
-	(--arch)
-	    if (( $# > 1 ));
-	    then { kcp_arch="$2"; shift; }
-	    else { echo "$0: missing architecture type" >&2; exit 1; }
-	    fi;;
-	(--folder)
-	    if (( $# > 1 ));
-	    then { kcp_folder="$2"; shift; }
-	    else { echo "$0: missing installation folder" >&2; exit 1; }
-	    fi;;
-	(--create-folder)
+    (--version)
+        if (( $# > 1 ));
+        then { kcp_version="$2"; shift; }
+        else { echo "$0: missing release version" >&2; exit 1; }
+        fi;;
+    (--os)
+        if (( $# > 1 ));
+        then { kcp_os="$2"; shift; }
+        else { echo "$0: missing OS type" >&2; exit 1; }
+        fi;;
+    (--arch)
+        if (( $# > 1 ));
+        then { kcp_arch="$2"; shift; }
+        else { echo "$0: missing architecture type" >&2; exit 1; }
+        fi;;
+    (--folder)
+        if (( $# > 1 ));
+        then { kcp_folder="$2"; shift; }
+        else { echo "$0: missing installation folder" >&2; exit 1; }
+        fi;;
+    (--create-folder)
         kcp_create_folder="true";;
-	(--verbose|-V)
+    (--verbose|-V)
         verbose="true";;
-	(-h|--help)
-	    echo "Usage: $0 [--version release_version] [--os linux|darwin] [--arch amd64|arm64] [--folder installation_folder] [--create-folder] [-V|--verbose]"
-	    exit 0;;
-	(-*)
-	    echo "$0: unknown flag" >&2 ; exit 1;
-	    exit 1;;
-	(*)
-	    echo "$0: unknown positional argument" >&2; exit 1;
-	    exit 1;;
+    (-h|--help)
+        echo "Usage: $0 [--version release_version] [--os linux|darwin] [--arch amd64|arm64] [--folder installation_folder] [--create-folder] [-V|--verbose]"
+        exit 0;;
+    (-*)
+        echo "$0: unknown flag" >&2 ; exit 1;
+        exit 1;;
+    (*)
+        echo "$0: unknown positional argument" >&2; exit 1;
+        exit 1;;
     esac
     shift
 done
@@ -102,54 +102,48 @@ if [ "$kcp_version" == "" ]; then
     kcp_version=$(get_latest_version)
 fi
 if [ "$kcp_os" == "" ]; then
-	kcp_os=$(get_os_type)
+    kcp_os=$(get_os_type)
 fi
 if [ "$kcp_arch" == "" ]; then
-	kcp_arch=$(get_arch_type)
+    kcp_arch=$(get_arch_type)
 fi
 if [ "$kcp_folder" == "" ]; then
     kcp_folder="$PWD/kcp"
 fi
 
-if [ ! -d "$kcp_folder" ]
-then
-    if [ "$kcp_create_folder" == "true" ]
-    then
-		if [ $verbose == "true" ]
-		then
-			echo "Creating folder: $kcp_folder"
-		fi
-        mkdir -p "$kcp_folder"
-    else
-        echo "Specified folder does not exist: $kcp_folder" >&2; exit 1;
+if [ -d "$kcp_folder" ]; then :
+elif [ "$kcp_create_folder" == "true" ]; then
+    if [ $verbose == "true" ] ; then
+        echo "Creating folder: $kcp_folder"
     fi
-fi
-
-if [ $verbose == "true" ]
-then
-	echo "Downloading KCP $kcp_version $kcp_os/$kcp_arch..."
-	curl -SL -o kcp.tar.gz "https://github.com/kcp-dev/kcp/releases/download/${kcp_version}/kcp_${kcp_version//v}_${kcp_os}_${kcp_arch}.tar.gz"
-	echo "Downloading KCP plugins $kcp_version $kcp_os/$kcp_arch..."
-	curl -SL -o kcp-plugins.tar.gz "https://github.com/kcp-dev/kcp/releases/download/${kcp_version}/kubectl-kcp-plugin_${kcp_version//v}_${kcp_os}_${kcp_arch}.tar.gz"
+    mkdir -p "$kcp_folder"
 else
-	curl -sSL -o kcp.tar.gz "https://github.com/kcp-dev/kcp/releases/download/${kcp_version}/kcp_${kcp_version//v}_${kcp_os}_${kcp_arch}.tar.gz"
-	curl -sSL -o kcp-plugins.tar.gz "https://github.com/kcp-dev/kcp/releases/download/${kcp_version}/kubectl-kcp-plugin_${kcp_version//v}_${kcp_os}_${kcp_arch}.tar.gz"
+    echo "Specified folder does not exist: $kcp_folder" >&2; exit 1;
 fi
 
 if [ $verbose == "true" ]
 then
-	echo "Extracting archive to: $kcp_folder"
+    echo "Downloading KCP $kcp_version $kcp_os/$kcp_arch..."
+    curl -SL -o kcp.tar.gz "https://github.com/kcp-dev/kcp/releases/download/${kcp_version}/kcp_${kcp_version//v}_${kcp_os}_${kcp_arch}.tar.gz"
+    echo "Downloading KCP plugins $kcp_version $kcp_os/$kcp_arch..."
+    curl -SL -o kcp-plugins.tar.gz "https://github.com/kcp-dev/kcp/releases/download/${kcp_version}/kubectl-kcp-plugin_${kcp_version//v}_${kcp_os}_${kcp_arch}.tar.gz"
+else
+    curl -sSL -o kcp.tar.gz "https://github.com/kcp-dev/kcp/releases/download/${kcp_version}/kcp_${kcp_version//v}_${kcp_os}_${kcp_arch}.tar.gz"
+    curl -sSL -o kcp-plugins.tar.gz "https://github.com/kcp-dev/kcp/releases/download/${kcp_version}/kubectl-kcp-plugin_${kcp_version//v}_${kcp_os}_${kcp_arch}.tar.gz"
+fi
+
+if [ $verbose == "true" ]; then
+    echo "Extracting archive to: $kcp_folder"
 fi
 tar -zxf kcp-plugins.tar.gz -C $kcp_folder
 tar -zxf kcp.tar.gz -C $kcp_folder
 
-if [ $verbose == "true" ]
-then
-	echo "Cleaning up..."
+if [ $verbose == "true" ]; then
+    echo "Cleaning up..."
 fi
 rm kcp.tar.gz
 rm kcp-plugins.tar.gz
 
 if [[ ! ":$PATH:" == *":$(get_full_path $kcp_folder/bin):"* ]]; then
-	echo "Add KCP folder to your path: export PATH="\$PATH:$(get_full_path $kcp_folder/bin)""
+    echo "Add KCP folder to your path: export PATH="\$PATH:$(get_full_path $kcp_folder/bin)""
 fi
