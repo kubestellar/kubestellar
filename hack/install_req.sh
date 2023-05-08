ccc11250a3081ce3f846266f50dede498101251f
Braulio Gabriel Dumba <brauliodumba@Braulios-MacBook-Pro.local>
2023-05-08 18:45:05 -0400

    moving scripts to the hack directory

---
+++ hack/install_req.sh
@@ -0,0 +1,216 @@
+#!/usr/bin/env bash
+# Copyright 2023 The KCP Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Purpose: to ensure that the required packages need to experiment with kcp-dge are installed
+
+# Assumption: supported OS Platforms Linux, MacOs or Windows WSL/Ubuntu
+
+# This will check if a package is already installed
+
+get_os_type() {
+  case "$OSTYPE" in
+      darwin*)  echo "darwin" ;;
+      linux*)   echo "linux" ;;
+      *)        echo "unknown: $OSTYPE" && exit 1 ;;
+  esac
+}
+
+os_type=$(get_os_type)
+
+# Install golang for x86_64
+if [ $os_type == "linux" ]; then
+    if !(command -v go) then
+        sudo apt update
+        sudo apt upgrade -y
+        curl -O https://dl.google.com/go/go1.19.2.linux-amd64.tar.gz
+        tar -xvf go1.19.2.linux-amd64.tar.gz
+        sudo mv go  /usr/bin/
+        sudo echo 'export GOROOT=/usr/bin/go' >> ~/.bashrc 
+        sudo echo 'export PATH=$PATH:$GOROOT/bin' >> ~/.bashrc 
+        sleep 5
+        exec bash
+    else
+        echo "Go is already installed"
+        echo "***********************"
+    fi
+elif [ $os_type == "darwin" ]; then
+    if !(command -v go) then
+        brew update
+        curl -O https://dl.google.com/go/go1.19.2.darwin-amd64.pkg
+        tar -xvf go1.19.2.darwin-amd64.pkg
+        sudo mv go  /usr/bin/
+        sudo echo 'export GOROOT=/usr/bin/go' >> ~/.bashrc 
+        sudo echo 'export PATH=$PATH:$GOROOT/bin' >> ~/.bashrc 
+        sleep 5
+        exec bash
+    else
+        echo "Go is already installed"
+        echo "***********************"
+    fi
+fi
+
+
+# Install make
+if [ $os_type == "linux" ]; then
+    if !(dpkg -l make) then
+        apt update
+        apt install -y make
+    else
+    echo "Make is already installed"
+    echo "*************************"
+    fi
+elif [ $os_type == "darwin" ]; then
+    if !(command -v make) then
+        brew update
+        brew install make
+    else
+        echo "Make is already installed"
+        echo "*************************"
+    fi
+fi
+
+
+# Install jq
+if [ $os_type == "linux" ]; then
+    if !(dpkg -l jq) then
+        apt update
+        apt install -y jq
+    else
+        echo "jq is already installed"
+        echo "****************************"
+    fi
+elif [ $os_type == "darwin" ]; then
+    if !(command -v jq) then
+        brew update
+        brew install jq
+    else
+        echo "jq is already installed"
+        echo "****************************"
+    fi
+fi
+
+
+# Install ko
+if [ $os_type == "linux" ]; then
+    if !(dpkg -l ko) then
+        apt update
+        VERSION=0.13.0 
+        OS=Linux
+        ARCH=x86_64
+        curl -sSfL "https://github.com/ko-build/ko/releases/download/v${VERSION}/ko_${VERSION}_${OS}_${ARCH}.tar.gz" > ko.tar.gz 
+        curl -sSfL https://github.com/ko-build/ko/releases/download/v${VERSION}/attestation.intoto.jsonl > provenance.intoto.jsonl
+        slsa-verifier -artifact-path ko.tar.gz -provenance provenance.intoto.jsonl -source github.com/google/ko -tag "v${VERSION}"
+        tar xzf ko.tar.gz ko
+        chmod +x ./ko
+        mv ./ko  /usr/local/bin/kind
+        rm ko.tar.gz
+    else
+        echo "ko is already installed"
+        echo "****************************"
+    fi
+elif [ $os_type == "darwin" ]; then
+    if !(command -v ko) then
+        brew update
+        brew install ko
+    else
+        echo "ko is already installed"
+        echo "****************************"
+    fi
+fi
+
+# Install gcc
+if [ $os_type == "linux" ]; then
+    if !(dpkg -l gcc) then
+        apt update
+        apt install -y build-essential
+    else
+        echo "gcc is already installed"
+        echo "****************************"
+    fi 
+elif [ $os_type == "darwin" ]; then
+    if !(command -v gcc) then
+        brew update
+        brew install gcc
+    else
+        echo "gcc is already installed"
+        echo "****************************"
+    fi 
+fi
+
+
+# Install Docker
+if [ $os_type == "linux" ]; then
+    if !(dpkg -l docker) then
+        sudo apt update -y
+        sudo apt install docker.io -y
+        sudo systemctl enable docker
+        sudo systemctl start docker
+    else
+        echo "docker is already installed"
+        echo "****************************"
+    fi
+elif [ $os_type == "darwin" ]; then
+     if !(command -v docker) then
+        echo "WARNING: PLEASE install Docker ....."
+     else
+        echo "docker is already installed"
+        echo "****************************"
+     fi
+fi
+
+
+# Install kubectl
+if [ $os_type == "linux" ]; then
+    if !(dpkg -l kubectl) then
+        sudo apt-get update
+        sudo apt-get install -y ca-certificates curl
+        sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+        echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+        sudo apt-get update
+        sudo apt-get install -y kubectl
+    else
+        echo "kubectl is already installed"
+        echo "****************************"
+    fi
+elif [ $os_type == "darwin" ]; then
+     if !(command -v kubectl) then
+        brew update
+        brew install kubectl
+     else
+        echo "kubectl is already installed"
+        echo "****************************"
+     fi
+fi
+
+
+# Install kind
+if [ $os_type == "linux" ]; then
+    if !(command -v kind) then
+        curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.16.0/kind-linux-amd64
+        chmod +x ./kind
+        sudo mv ./kind /usr/local/bin/kind
+    else
+        echo "kind is already installed"
+        echo "*************************"
+    fi
+elif [ $os_type == "darwin" ]; then
+     if !(command -v kind) then
+        brew update
+        brew install kind
+     else
+        echo "kind is already installed"
+        echo "*************************"
+     fi
+fi
---
+++ hack/kcp-edge.sh
@@ -0,0 +1,252 @@
+#!/usr/bin/env bash
+# Copyright 2023 The KCP Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Usage: $0 (start or stop | --user dev or kit | -v
+
+# Purpose: deploy the kcp-edge platform. The following components are created"
+#           (a) 1 kcp workspace: edge service provider workspace (espw)
+#           (b) 3 kcp-edge controllers: edge-scheduler, mailbox-controller and placement-translator
+
+# Assumption: kcp server is running.
+
+# This script requires the edge-mc binaries mailbox-controller, scheduler, placement-translator
+# already exist at edge-mc/bin/
+
+# This script requires that the parent directory is edge-mc to run in dev mode
+
+
+set -e
+
+install=""
+verbosity=0
+espw_name="espw"
+user_type="dev"
+
+while (( $# > 0 )); do
+    if [ "$1" == "start" ]; then
+        install=1
+    elif [ "$1" == "stop" ]; then
+        install=0
+    elif [ "$1" == "-v" ]; then
+        verbosity=1
+    elif [ "$1" == "--user" ]; then
+        user_type=$2
+    fi 
+    shift
+done
+
+# Check if docker is running
+if ! docker ps > /dev/null
+then
+  echo "Docker Not running ...."
+  exit
+fi
+
+# Check go version
+go_version=`go version | { read _ _ v _; echo ${v#go}; }`
+
+function ver { printf "%03d%03d%03d%03d" $(echo "$1" | tr '.' ' '); }
+if [ $(ver $go_version) -lt $(ver 1.19) ]; then
+    echo "Update your go version"
+fi
+
+# Check if a given process name is running
+process_running() {
+  SERVICE="$1"
+  if pgrep -f "$SERVICE" >/dev/null
+  then
+      echo "running"
+  else
+      echo "stopped" 
+  fi
+}
+
+# Check if kcp is running
+if [ $(process_running kcp) != "running" ]
+then
+    echo "kcp is not running - please start it ...."
+    exit
+fi
+
+# Check mailbox-controller is already running
+if [ $(process_running mailbox-controller) == "running" ]
+then
+    echo "An older deployment of mailbox-controller is already running - terminating it ...."
+    pkill -f mailbox-controller
+fi
+
+# Check edge-scheduler is already running
+if [ $(process_running "scheduler -v 2") == "running" ]
+then
+    echo "An older deployment of edge-scheduler is already running - terminating it ...."
+    pkill -f "scheduler -v 2"
+fi
+
+# Check placement-translator is already running
+if [ $(process_running placement-translator) == "running" ]
+then
+    echo "An older deployment of placement-translator is already running - terminating it ...."
+    pkill -f placement-translator
+fi
+
+kubectl ws root &> /dev/null
+
+if [ $install -eq 0 ]; then 
+   if kubectl get Workspace "$espw_name" &> /dev/null; then
+      kubectl delete ws "$espw_name"
+      echo "Deleted workspace: $espw_name"
+   fi
+   echo "kcp edge stopped ....."
+   exit
+fi
+
+
+wait_for_process(){
+  status=$(process_running $1)
+  MAX_RETRIES=5
+  retries=0
+  status_code=0
+  while [ $status != "running" ]; do
+      if [ $retries -eq $MAX_RETRIES ]; then
+           status_code=1
+           break
+      fi
+
+      retries=$(( retries + 1 ))
+      sleep 3
+      status=$(process_running $1)
+  done
+  echo $status_code
+}
+
+
+#(7): Start the edge-mc controller
+echo "****************************************"
+echo "Started deploying kCP-EDGE infra ...."
+echo "****************************************"
+
+
+if kubectl get Workspace "$espw_name" &> /dev/null; then
+   echo "espw workspace already exists -- using it:"
+   kubectl ws "$espw_name"
+else 
+   if [ $verbosity == 1 ]; then
+        kubectl ws create "$espw_name" --enter
+
+        if [ $user_type == "dev" ]; then
+            kubectl apply -f  ../config/crds 
+            kubectl apply -f  ../config/exports
+            echo "Finished populate the espw with kcp edge crds and apiexports"
+
+        elif [ $user_type == "kit" ]; then
+            # Apply CRDs
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/crds/edge.kcp.io_customizers.yaml
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/crds/edge.kcp.io_edgeplacements.yaml
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/crds/edge.kcp.io_edgesyncconfigs.yaml
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/crds/edge.kcp.io_singleplacementslices.yaml
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/crds/edge.kcp.io_syncerconfigs.yaml
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/crds/meta.kcp.io_apiresources.yaml
+
+            # Apply Exports
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/exports/apiexport-edge.kcp.io.yaml
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/exports/apiexport-meta.kcp.io.yaml
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/exports/apiresourceschema-apiresources.meta.kcp.io.yaml
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/exports/apiresourceschema-customizers.edge.kcp.io.yaml
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/exports/apiresourceschema-edgeplacements.edge.kcp.io.yaml
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/exports/apiresourceschema-edgesyncconfigs.edge.kcp.io.yaml
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/exports/apiresourceschema-singleplacementslices.edge.kcp.io.yaml
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/exports/apiresourceschema-syncerconfigs.edge.kcp.io.yaml
+            echo "Finished populate the espw with kcp edge crds and apiexports"
+        else
+            echo "Unknown user type ..."
+            exit 1
+        fi 
+   else
+        kubectl ws create "$espw_name" --enter
+
+        if [ $user_type == "dev" ]; then
+            kubectl apply -f  ../config/crds 
+            kubectl apply -f  ../config/exports
+            echo "Finished populate the espw with kcp edge crds and apiexports"
+
+        elif [ $user_type == "kit" ]; then
+            # Apply CRDs
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/crds/edge.kcp.io_customizers.yaml  &> /dev/null
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/crds/edge.kcp.io_edgeplacements.yaml &> /dev/null
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/crds/edge.kcp.io_edgesyncconfigs.yaml &> /dev/null
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/crds/edge.kcp.io_singleplacementslices.yaml &> /dev/null
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/crds/edge.kcp.io_syncerconfigs.yaml &> /dev/null
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/crds/meta.kcp.io_apiresources.yaml &> /dev/null
+
+            # Apply Exports
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/exports/apiexport-edge.kcp.io.yaml &> /dev/null
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/exports/apiexport-meta.kcp.io.yaml &> /dev/null
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/exports/apiresourceschema-apiresources.meta.kcp.io.yaml &> /dev/null
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/exports/apiresourceschema-customizers.edge.kcp.io.yaml  &> /dev/null
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/exports/apiresourceschema-edgeplacements.edge.kcp.io.yaml &> /dev/null
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/exports/apiresourceschema-edgesyncconfigs.edge.kcp.io.yaml &> /dev/null
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/exports/apiresourceschema-singleplacementslices.edge.kcp.io.yaml &> /dev/null
+            kubectl apply -f https://raw.githubusercontent.com/kcp-dev/edge-mc/main/config/exports/apiresourceschema-syncerconfigs.edge.kcp.io.yaml &> /dev/null
+            echo "Finished populate the espw with kcp edge crds and apiexports"
+        else
+            echo "Unknown user type ..."
+            exit 1
+        fi 
+   fi
+fi
+
+sleep 5
+
+mailbox-controller --inventory-context=root --mbws-context=base -v=2 >& mailbox-controller-log.txt &
+
+run_status=$(wait_for_process mailbox-controller)
+if [ $run_status -eq 0 ]; then
+    echo " mailbox-controller is running (log file: mailbox-controller-log.txt)"
+else
+    echo " mailbox-controller failed to start ..... exiting"
+    sleep 2
+    exit
+fi
+
+
+# Start the edge-mc scheduler
+sleep 3
+scheduler -v 2 --root-user kcp-admin  --root-cluster root  --sysadm-context system:admin  --sysadm-user shard-admin >& edge-scheduler-log.txt &
+
+run_status=$(wait_for_process "scheduler -v 2")
+if [ $run_status -eq 0 ]; then
+    echo " scheduler is running (log file: edge-scheduler-log.txt)"
+else
+    echo " scheduler failed to start ..... exiting"
+    exit
+fi
+ 
+# Start the Placement Translator
+sleep 3
+placement-translator --allclusters-context  "system:admin" -v=2 >& placement-translator-log.txt &
+
+run_status=$(wait_for_process placement-translator)
+if [ $run_status -eq 0 ]; then
+    echo " placement translator is running (log file: placement-translator-log.txt)"
+else
+    echo " placement translator failed to start ..... exiting"
+    exit
+fi
+
+sleep 10
+echo "****************************************"
+echo "Finished deploying kCP-EDGE controllers ...."
+echo "****************************************"
+kubectl ws root
