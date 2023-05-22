035ed347dee229c958dc7fa8de4d07e9013f0419
Andy Anderson <andy@clubanderson.com>
2023-05-22 17:40:47 -0400

    Update declarations.go

--- pkg/placement/declarations.go
+++ pkg/placement/declarations.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 The KubeStellar Authors Authors Authors Authors Authors Authors Authors.
+Copyright 2023 The KubeStellar Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
