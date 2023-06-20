369b6b366b75694f9563be5d795c307017ccbf3a
andreyod <andreyo@il.ibm.com>
2023-06-20 04:26:59 +0300

    Convert to contextual logging.
    Signed-off-by: andreyod <andreyo@il.ibm.com>

--- cmd/mc-client-test/main.go
+++ cmd/mc-client-test/main.go
@@ -22,7 +22,7 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/clientcmd"
-	log "k8s.io/klog/v2"
+	klog "k8s.io/klog/v2"
 	kind "sigs.k8s.io/kind/pkg/cluster"
 
 	lcv1 "github.com/kcp-dev/edge-mc/pkg/apis/logicalcluster/v1alpha1"
@@ -41,33 +41,38 @@ const defaultProviderNs = "lcprovider-default"
 // go run main.go
 func main() {
 	ctx := context.Background()
+	logger := klog.Background()
+	ctx = klog.NewContext(ctx, logger)
 
 	kubeConfigPath := os.Getenv("HOME") + "/.kube/config"
 	managementClusterConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
 	if err != nil {
-		log.Fatalf("failed to build management cluster config: %v", err)
+		logger.Error(err, "failed to build management cluster config")
+		panic(err)
 	}
 	managementksClientset, err := ksclientset.NewForConfig(managementClusterConfig)
 	if err != nil {
-		log.Fatalf("failed to build management cluster clientset: %v", err)
+		logger.Error(err, "failed to build management cluster clientset")
+		panic(err)
 	}
 
 	createClusterObjectKS(ctx, managementksClientset, "cluster1")
 
 	mcclient, err := mcclient.NewMultiCluster(ctx, managementClusterConfig)
 	if err != nil {
-		log.Fatalf("get client failed: %v", err)
+		logger.Error(err, "get client failed")
+		panic(err)
 	}
 
-	log.Info("List EdgePlacements in cluster1")
+	logger.Info("List EdgePlacements in cluster1")
 	list, _ := mcclient.Cluster("cluster1").KS().EdgeV1alpha1().EdgePlacements().List(ctx, metav1.ListOptions{})
 	for _, ep := range list.Items {
-		log.Infof("Cluster: cluster1 ; ObjName: %s", ep.Name)
+		logger.Info("Cluster: cluster1", "EdgePlacement", ep.Name)
 	}
-	log.Info("List configmaps in cluster1")
+	logger.Info("List configmaps in cluster1")
 	listm, _ := mcclient.Cluster("cluster1").Kube().CoreV1().ConfigMaps(metav1.NamespaceDefault).List(ctx, metav1.ListOptions{})
-	for _, ep := range listm.Items {
-		log.Infof("Cluster: cluster1 ; ObjName: %s", ep.Name)
+	for _, cm := range listm.Items {
+		logger.Info("Cluster: cluster1", "ConfigMap", cm.Name)
 	}
 
 	updateClusterObjectKS(ctx, managementksClientset, "cluster1")
@@ -75,10 +80,11 @@ func main() {
 }
 
 func createClusterObjectKS(ctx context.Context, clientset ksclientset.Interface, cluster string) {
+	logger := klog.FromContext(ctx)
 	provider := kind.NewProvider()
 	kubeconfig, err := provider.KubeConfig(cluster, false)
 	if err != nil {
-		log.Error("failed to get config from kind provider")
+		logger.Error(err, "failed to get config from kind provider")
 		return
 	}
 
@@ -98,33 +104,35 @@ func createClusterObjectKS(ctx context.Context, clientset ksclientset.Interface,
 
 	_, err = clientset.LogicalclusterV1alpha1().LogicalClusters(defaultProviderNs).Create(ctx, &cm, metav1.CreateOptions{})
 	if err != nil {
-		log.Errorf("failed to create LogicalCluster: %v", err)
+		logger.Error(err, "failed to create LogicalCluster")
 		return
 	}
-	log.Infof("LogicalCluster created: %s", cluster)
+	logger.Info("LogicalCluster created", "cluster", cluster)
 }
 
 func deleteClusterObjectKS(ctx context.Context, clientset ksclientset.Interface, cluster string) {
+	logger := klog.FromContext(ctx)
 	err := clientset.LogicalclusterV1alpha1().LogicalClusters(defaultProviderNs).Delete(ctx, cluster, metav1.DeleteOptions{})
 	if err != nil {
-		log.Errorf("failed to delete LogicalCluster: %v", err)
+		logger.Error(err, "failed to delete LogicalCluster")
 		return
 	}
-	log.Infof("LogicalCluster deleted: %s", cluster)
+	logger.Info("LogicalCluster deleted", "cluster", cluster)
 }
 
 func updateClusterObjectKS(ctx context.Context, clientset ksclientset.Interface, cluster string) {
+	logger := klog.FromContext(ctx)
 	clusterConfig, err := clientset.LogicalclusterV1alpha1().LogicalClusters(defaultProviderNs).Get(ctx, cluster, metav1.GetOptions{})
 	if err != nil {
-		log.Errorf("failed to get LogicalCluster: %v", err)
+		logger.Error(err, "failed to get LogicalCluster")
 		return
 	}
 
 	clusterConfig.Status.Phase = lcv1.LogicalClusterPhaseNotReady
 	_, err = clientset.LogicalclusterV1alpha1().LogicalClusters(defaultProviderNs).Update(ctx, clusterConfig, metav1.UpdateOptions{})
 	if err != nil {
-		log.Errorf("failed to update LogicalCluster: %v", err)
+		logger.Error(err, "failed to update LogicalCluster")
 		return
 	}
-	log.Infof("LogicalCluster updated: %s", cluster)
+	logger.Info("LogicalCluster updated", "cluster", cluster)
 }
