afb6cc38ae8ecafbf186572e04615446e30bd168
Takumi Yanagawa <yana@jp.ibm.com>
2023-11-28 11:56:35 +0900

    correct copy rights (#1315)

--- test/e2e/framework/util.go
+++ test/e2e/framework/util.go
@@ -16,9 +16,25 @@ limitations under the License.
 
 /*
 Portions of this code are based on or inspired by the KCP Author's work
-Copyright Copyright 2022 The KCP Authors
-Original code: https://github.com/kcp-dev/kcp/blob/release-0.11/test/e2e/framework/util.go,
+The original code is https://github.com/kcp-dev/kcp/blob/release-0.11/test/e2e/framework/util.go,
 https://github.com/kcp-dev/kcp/blob/release-0.11/test/e2e/framework/kubectl.go
+The original copyright is as follows:
+*/
+
+/*
+Copyright 2022 The KCP Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 package framework
