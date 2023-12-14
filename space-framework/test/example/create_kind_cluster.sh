14e9280dce39a136d26e743a2a9f1513b56e57f5
Ezra Silvera <ezra@il.ibm.com>
2023-12-14 19:42:52 +0200

    boilertap
    Signed-off-by: Ezra Silvera <ezra@il.ibm.com>

--- space-framework/test/example/create_kind_cluster.sh
+++ space-framework/test/example/create_kind_cluster.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# Copyright 2023 The KubeStellar Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 # Create a kind cluster which will be used to host the space manager and kubeflex. 
 # We could create two separate clusters, one for each, but for convenience we create one. 
 # Note that kubeflex requires ingress, so we also need to set that up.
