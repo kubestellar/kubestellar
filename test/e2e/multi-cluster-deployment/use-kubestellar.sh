da3b7d83c70ef8ce265016cfcbd47da31f79d2c0
Maroon Ayoub <maroon.ayoub@ibm.com>
2024-03-12 13:07:56 +0200

    revert test changes and instead append a new selectors check

--- test/e2e/multi-cluster-deployment/use-kubestellar.sh
+++ test/e2e/multi-cluster-deployment/use-kubestellar.sh
@@ -28,9 +28,7 @@ metadata:
   name: nginx-bindingpolicy
 spec:
   clusterSelectors:
-  - matchLabels: {"name":"cluster1"}
-  - matchExpressions:
-    - { key: name, operator: In, values: [cluster2] }
+  - matchLabels: {"location-group":"edge"}
   downsync:
   - objectSelectors:
     - matchLabels: {"app.kubernetes.io/name":"nginx"}
