a5c4c5844a06d9f279bc46345851b69379265810
Mike Spreitzer <mspreitz@us.ibm.com>
2024-03-15 01:52:24 -0400

    Remove obsolete command from E2E cleanup script
    Signed-off-by: Mike Spreitzer <mspreitz@us.ibm.com>

--- test/e2e/common/cleanup.sh
+++ test/e2e/common/cleanup.sh
@@ -15,7 +15,6 @@
 
 set -x # echo so that users can understand what is happening
 set -e # exit on error
-SRC_DIR="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 
 :
 : -------------------------------------------------------------------------
