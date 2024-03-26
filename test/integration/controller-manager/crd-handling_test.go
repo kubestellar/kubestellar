611837e7f0119f6fe2983d14551222f47df7c7ab
Jun Duan <jun.duan.phd@outlook.com>
2024-03-26 18:53:41 +0000

    Do full discovery only once

--- test/integration/controller-manager/crd-handling_test.go
+++ test/integration/controller-manager/crd-handling_test.go
@@ -93,6 +93,11 @@ func TestCRDHandling(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Log("CRDs ensured")
+	err = ctlr.AppendKSResources(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("Appended KS resources to discovered lists")
 	err = ctlr.Start(ctx, 4, make(chan interface{}, 1))
 	if err != nil {
 		t.Fatal(err)
