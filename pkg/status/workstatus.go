f1eb9d2c30792456c282f531f4bf5efcdb91df19
Jun Duan <jun.duan.phd@outlook.com>
2024-08-28 14:08:04 +0000

    Add missing lister for cluster-scoped workload object when updating status

--- pkg/status/workstatus.go
+++ pkg/status/workstatus.go
@@ -90,17 +90,22 @@ func updateObjectStatus(ctx context.Context, objectIdentifier *util.ObjectIdenti
 	gvr := objectIdentifier.GVR()
 	lister, found := listers.Get(gvr)
 	if !found {
-		logger.V(2).Info("Could not find lister for gvr", "gvr", gvr)
+		logger.V(4).Info("Could not find lister for gvr", "gvr", gvr)
 		return nil
 	}
 
-	obj, err := lister.ByNamespace(objectIdentifier.ObjectName.Namespace).Get(objectIdentifier.ObjectName.Name)
+	var obj runtime.Object
+	var err error
+	if objectIdentifier.ObjectName.Namespace == "" {
+		obj, err = lister.Get(objectIdentifier.ObjectName.Name)
+	} else {
+		obj, err = lister.ByNamespace(objectIdentifier.ObjectName.Namespace).Get(objectIdentifier.ObjectName.Name)
+	}
 	if err != nil {
 		if errors.IsNotFound(err) {
-			logger.V(2).Info("Could not find object", "objectIdentifier", objectIdentifier)
+			logger.V(4).Info("Did not update workload object because it is not in local cache, presumably because it was recently deleted", "objectIdentifier", objectIdentifier)
 			return nil
 		}
-
 		return fmt.Errorf("failed to get object (%v): %w", objectIdentifier, err)
 	}
 
