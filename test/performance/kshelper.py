0dd432911ceb96ff15bd653f2d94377421b0a758
Braulio Dumba <brauliodumba@gmail.com>
2024-08-28 22:15:28 -0400

    Adding scripts to collect timestamp metrics from the experiment workloads
    Signed-off-by: Braulio Dumba <brauliodumba@gmail.com>

--- test/performance/kshelper.py
+++ test/performance/kshelper.py
@@ -27,11 +27,10 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 class Collector():
-    def __init__(self, kubeconfig, k8s_context, fout):
-        self.fout = fout
+    def __init__(self, kubeconfig, k8s_context):
         self.kubeconfig = kubeconfig
         self.context = k8s_context
-        self.core_v1_api, self.custom_objects_api = self.load_kubeconfig(kubeconfig, k8s_context)
+        self.core_v1_api, self.app_v1_api, self.custom_objects_api = self.load_kubeconfig(kubeconfig, k8s_context)
 
     def load_kubeconfig(self, kubeconfig, context):
         kube_config_orig = kubeconfig
@@ -51,48 +50,58 @@ class Collector():
         
         api_client = kubernetes.client.ApiClient()
         core_v1_api = kubernetes.client.CoreV1Api(api_client)
+        app_v1_api  = kubernetes.client.AppsV1Api(api_client)
         custom_objects_api = kubernetes.client.CustomObjectsApi(api_client)
 
         os.remove(tmp_config)
-        return core_v1_api, custom_objects_api
+        return core_v1_api, app_v1_api, custom_objects_api
 
-    def get_crd_time(self, grp, v, pl, name=None, check=False):
+    def get_custom_objects_time(self, fname, grp, v, pl, name=None, ns=None, bindingPolicy=None, check=False, single=True):
         try:
-            if name != None and check == False:
-                api_response = self.custom_objects_api.list_cluster_custom_object(group=grp, version=v, plural=pl, field_selector='metadata.name=' + name)
+            if ns != None and bindingPolicy != None:
+               api_response = self.custom_objects_api.list_namespaced_custom_object(group=grp, version=v, plural=pl, namespace=ns, label_selector='transport.kubestellar.io/originOwnerReferenceBindingKey=' + bindingPolicy)
+            elif name != None and ns != None and bindingPolicy == None:
+               api_response = self.custom_objects_api.get_namespaced_custom_object(group=grp, version=v, namespace=ns, plural=pl, name=name)
+            elif name != None and ns == None and bindingPolicy == None and check==False:
+               api_response = self.custom_objects_api.get_cluster_custom_object(group=grp, version=v, plural=pl, name=name)
             else:
-                api_response = self.custom_objects_api.list_cluster_custom_object(group=grp, version=v, plural=pl)
+               api_response = self.custom_objects_api.list_cluster_custom_object(group=grp, version=v, plural=pl)
 
-            object_list = api_response["items"]
-            if check: # temporary fix to collect Workstatus objects - it must be improved later to optimize performance
+            if name == None or single == False:
+               # Create the output file
+               fout = open(fname + ".csv", 'a')
+
+            if "items" in api_response:
+               object_list = api_response["items"]
+            else:
+               object_list = [api_response]
+            
+            if check: # temporary fix - it must be improved later to optimize performance
                 for i in range(0, len(object_list)):
                     temp_name = object_list[i]["metadata"]["name"]
                     if name in temp_name:
                         object_list = [object_list[i]]
                         break
 
-            creation_time = ""
-            update_time = ""
+            creation_timestamp = ""
+            update_timestamp = ""
             status_condition = ""
             op_manager = ""
-            
-            dict_manager_list = ["controller-manager", "ocm-status-addon", "kubelet", "work"]
-            if os.path.exists(self.fout):
-                mode = 'a' # append if already exists
-            else:
-                mode = 'w' # make a new file if not
-
-            fout = open(self.fout, mode)
+            manager_list = ["controller-manager", "ocm-status-addon", "kubelet", "work", "Go-http-client", "registration-operator", "transport-controller"]
 
             if object_list:
                 for i in range(0, len(object_list)):
-                    name = object_list[i]["metadata"]["name"]
-                    creation_time = object_list[i]["metadata"]["creationTimestamp"]
+                    obj_name = object_list[i]["metadata"]["name"]
+                    creation_timestamp = object_list[i]["metadata"]["creationTimestamp"]
 
                     if "status" in object_list[i]:
                         status = object_list[i]["status"]
                     else:
-                        fout.write(name + "\t" + creation_time + "\t" + update_time + "\t" + status_condition + "\t" + op_manager + "\n")
+                        output=obj_name + "\t" + creation_timestamp + "\t" + update_timestamp + "\t" + status_condition + "\t" + op_manager
+                        if name != None and single == True:
+                           return output
+                        else:
+                           fout.write(output + "\n")
                         continue
 
                     if "phase" in status:
@@ -106,72 +115,139 @@ class Collector():
                         op_type = "subresource"
 
                         if operation == "Update":
-                            if manager in dict_manager_list:
-                                if op_type in managedFields[j]:
-                                    update_time = managedFields[j]["time"]
-                                    op_manager = manager
-                                    break
+                            if manager in manager_list:
+                                if pl == "manifestworks": 
+                                   # Handle manifestwork objects differently: extract the timestamp when transport-controller updates a manifestwork object 
+                                   if manager == "transport-controller":
+                                      update_timestamp = managedFields[j]["time"]
+                                      op_manager = manager
+                                      break
+                                   else:
+                                     continue
+                                elif op_type in managedFields[j]:
+                                     update_timestamp = managedFields[j]["time"]
+                                     op_manager = manager
+                                     break
                         else:
-                            continue 
-                    fout.write(name + "\t" + creation_time + "\t" + update_time + "\t" + status_condition + "\t" + op_manager + "\n")
-            fout.close()
+                            continue
+
+                    output=obj_name + "\t" + creation_timestamp + "\t" + update_timestamp + "\t" + status_condition + "\t" + op_manager
+                    if name != None and single == True:
+                       return output
+                    else:
+                       fout.write(output+ "\n")
+
+            if name == None or single == False:
+               fout.flush()
+               fout.close()
+            return None
+
         except ApiException as e:
             print("Exception when calling client->CustomObjectsApi(): %s\n" % e)
 
 
-    def get_pod_time(self, ns, name=None):
+    def get_obj_time(self, fname, kind, ns, name=None):
         try:
+            kind = kind.lower()
             if name != None:
-                api_response = self.core_v1_api.list_namespaced_pod(ns, field_selector='metadata.name=' + name)
-            else: 
-                api_response = self.core_v1_api.list_namespaced_pod(ns)
+                if kind == "pod" or kind == "pods":
+                   api_response = self.core_v1_api.read_namespaced_pod(name, ns)
+                elif kind == "deployment" or kind == "deployments":
+                   api_response = self.app_v1_api.read_namespaced_deployment(name, ns)
+                elif kind == "secret" or kind == "secrets":
+                   api_response = self.core_v1_api.read_namespaced_secret(name, ns)
+                elif kind == "configmap" or kind == "configmaps":
+                   api_response = self.core_v1_api.read_namespaced_config_map(name, ns)
+                elif kind == "service" or kind == "services":
+                   api_response = self.core_v1_api.read_namespaced_service(name, ns)
+                else:
+                    print("Not supported object kind ...")
+                    return
+                object_list = [api_response]
+            else:
+                if kind == "pod" or kind == "pods": 
+                   api_response = self.core_v1_api.list_namespaced_pod(ns)
+                elif kind == "deployment" or kind == "deployments":
+                   api_response = self.app_v1_api.list_namespaced_deployment(ns)
+                elif kind == "secret" or kind == "secrets":
+                   api_response = self.core_v1_api.list_namespaced_secret(ns)
+                elif kind == "configmap" or kind == "configmaps":
+                   api_response = self.core_v1_api.list_namespaced_config_map(ns)
+                elif kind == "service" or kind == "services":
+                   api_response = self.core_v1_api.list_namespaced_service(ns)
+                else:
+                    print("Not supported object kind ...")
+                    return
+                object_list = api_response.items
+                
+                # Create the output file
+                fout = open(fname + ".csv", 'a')
 
-            object_list = api_response.items
-
-            creation_time = ""
-            update_time = ""
+            creation_timestamp = ""
+            update_timestamp = ""
             status_condition = ""
             op_manager = ""
-
-            dict_manager_list = ["controller-manager", "ocm-status-addon", "kubelet", "k3s"]
-            if os.path.exists(self.fout):
-                mode = 'a' # append if already exists
-            else:
-                mode = 'w' # make a new file if not
-
-            fout = open(self.fout, mode)
+            manager_list = ["kube-controller-manager", "controller-manager", "ocm-status-addon", "kubelet", "k3s"]
 
             if object_list:
                 for i in range(0, len(object_list)):
-                    name = object_list[i].metadata.name
-                    creation_time = object_list[i].metadata.creation_timestamp
+                    obj_name = object_list[i].metadata.name
+                    creation_timestamp = object_list[i].metadata.creation_timestamp
+
+                    if (kind != "pod" and kind != "pods") and (kind != "deployment" and kind != "deployments") and (kind != "service" and kind != "services"):
+                       output=obj_name + "\t" + str(creation_timestamp)
+                       if name != None:
+                          return output
+                       else:
+                          fout.write(output + "\n")
+                       continue
 
                     if object_list[i].status:
-                        status = object_list[i].status
+                       status = object_list[i].status
                     else:
-                        fout.write(name + "\t" + creation_time + "\t" + update_time + "\t" + status_condition + "\t" + op_manager + "\n")
+                        output=obj_name + "\t" + str(creation_timestamp) + "\t" + str(update_timestamp) + "\t" + status_condition + "\t" + op_manager
+                        if name != None:
+                           return output
+                        else:
+                          fout.write(output+ "\n")
                         continue
-
-                    if status.phase:
-                        status_state = status.phase
-                        status_condition = status_state
                     
+                    if kind == "pod" or kind == "pods":
+                       if status.phase:
+                          status_condition = status.phase
+                    if kind == "deployment" or kind == "deployments":
+                       replicas = status.replicas
+                       readyReplicas = status.ready_replicas
+
+                       if replicas == readyReplicas:
+                          status_condition = "Available"
+                       else:
+                          status_condition = "Unavailable"                   
+                       
                     # Get update timestamp for subresource of type "time"
                     managedFields = object_list[i].metadata.managed_fields
                     for j in range(0, len(managedFields)):
-
                         operation = managedFields[j].operation
                         manager = managedFields[j].manager 
                         op_type =  managedFields[j].subresource
 
                         if operation == "Update" and op_type == "status":
-                            if manager in dict_manager_list:
-                                update_time = managedFields[j].time
-                                op_manager = manager
-                                break
+                            if manager in manager_list:
+                               update_timestamp = managedFields[j].time
+                               op_manager = manager
+                               break
                         else:
                             continue 
-                    fout.write(name + "\t" + str(creation_time) + "\t" + str(update_time) + "\t" + status_condition + "\t" + op_manager + "\n")
-            fout.close()
+                    
+                    output = obj_name + "\t" + str(creation_timestamp) + "\t" + str(update_timestamp) + "\t" + status_condition + "\t" + op_manager
+                    if name != None:
+                       return output
+                    else: 
+                       fout.write(output + "\n")
+
+            if name == None:
+               fout.flush()
+               fout.close()
+            return None
         except ApiException as e:
-            print("Exception when calling CoreV1Api->list_namespaced_pod: %s\n" % e)
+            print("Exception when calling CoreV1Api/AppV1Api: %s\n" % e)
