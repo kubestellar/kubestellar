e25892117cde6736186059c6f375013c677bcd01
Mike Spreitzer <mspreitz@us.ibm.com>
2024-10-21 16:55:42 -0400

    Add doc for getting started from OCM
    Signed-off-by: Mike Spreitzer <mspreitz@us.ibm.com>

---
+++ scripts/ocm-local-up-for-ingress.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+cd $(dirname ${BASH_SOURCE})
+
+set -e
+
+hub=${CLUSTER1:-hub}
+c1=${CLUSTER1:-cluster1}
+c2=${CLUSTER2:-cluster2}
+
+hubctx="kind-${hub}"
+c1ctx="kind-${c1}"
+c2ctx="kind-${c2}"
+
+kind create cluster --name "${hub}" --config - <<EOF
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+  extraPortMappings:
+  - containerPort: 443
+    hostPort: 9443
+    protocol: TCP
+EOF
+echo "Installing an nginx ingress controller into the hub cluster..."
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+echo "Pathcing nginx ingress controller to enable SSL passthrough..."
+kubectl patch deployment ingress-nginx-controller -n ingress-nginx -p '{"spec":{"template":{"spec":{"containers":[{"name":"controller","args":["/nginx-ingress-controller","--election-id=ingress-nginx-leader","--controller-class=k8s.io/ingress-nginx","--ingress-class=nginx","--configmap=$(POD_NAMESPACE)/ingress-nginx-controller","--validating-webhook=:8443","--validating-webhook-certificate=/usr/local/certificates/cert","--validating-webhook-key=/usr/local/certificates/key","--watch-ingress-without-class=true","--publish-status-address=localhost","--enable-ssl-passthrough"]}]}}}}'
+
+kind create cluster --name "${c1}"
+kind create cluster --name "${c2}"
+
+echo "Waiting for nginx ingress controller with SSL passthrough to be ready..."
+while [ -z "$(kubectl --context ${hubctx} get pod --namespace ingress-nginx --selector=app.kubernetes.io/component=controller --no-headers -o name 2> /dev/null)" ] ;  do
+    sleep 5
+done
+kubectl --context ${hubctx} wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
+
+echo "Initialize the ocm hub cluster\n"
+clusteradm init --wait --context ${hubctx}
+joincmd=$(clusteradm get token --context ${hubctx} | grep clusteradm)
+
+echo "Join cluster1 to hub\n"
+$(echo ${joincmd} --force-internal-endpoint-lookup --wait --context ${c1ctx} | sed "s/<cluster_name>/$c1/g")
+
+echo "Join cluster2 to hub\n"
+$(echo ${joincmd} --force-internal-endpoint-lookup --wait --context ${c2ctx} | sed "s/<cluster_name>/$c2/g")
+
+echo "Accept join of cluster1 and cluster2"
+clusteradm accept --context ${hubctx} --clusters ${c1},${c2} --wait
+
+kubectl get managedclusters --all-namespaces --context ${hubctx}
