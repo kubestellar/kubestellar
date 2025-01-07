da99ab4f6a4319c5051b384b707dd0974185a486
RealRTorres <riane.torres@gmail.com>
2025-01-07 13:43:46 -0500

    :sparkles: Update create-kind-cluster-with-SSL-passthrough.sh
    Found typo and corrected it
    
    Signed-off-by: Riane Torres <riane.torres@gmail.com>

--- scripts/create-kind-cluster-with-SSL-passthrough.sh
+++ scripts/create-kind-cluster-with-SSL-passthrough.sh
@@ -142,7 +142,7 @@ else
   kubectl --context "kind-${name}" apply -f "$NGINX_INGRESS_URL"
 fi
 
-echo "Pathcing nginx ingress to enable SSL passthrough..."
+echo "Patching nginx ingress to enable SSL passthrough..."
 kubectl --context "kind-${name}" patch deployment ingress-nginx-controller -n ingress-nginx -p '{"spec":{"template":{"spec":{"containers":[{"name":"controller","args":["/nginx-ingress-controller","--election-id=ingress-nginx-leader","--controller-class=k8s.io/ingress-nginx","--ingress-class=nginx","--configmap=$(POD_NAMESPACE)/ingress-nginx-controller","--validating-webhook=:8443","--validating-webhook-certificate=/usr/local/certificates/cert","--validating-webhook-key=/usr/local/certificates/key","--watch-ingress-without-class=true","--publish-status-address=localhost","--enable-ssl-passthrough"]}]}}}}'
 
 if [[ "$wait" == "true" ]] ; then
