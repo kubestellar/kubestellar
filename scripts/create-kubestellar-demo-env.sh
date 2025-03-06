5dcee7ddcf0250fd3a86eead4a7a015ab5115f54
Mike Spreitzer <mspreitz@us.ibm.com>
2025-03-06 15:23:23 -0500

    Remove another pull from DockerHub
    .. because of its annoying rate limiting.

--- scripts/create-kubestellar-demo-env.sh
+++ scripts/create-kubestellar-demo-env.sh
@@ -192,7 +192,7 @@ echo -e "\nPulling container images local..."
 images=("ghcr.io/loft-sh/vcluster:0.16.4"
         "rancher/k3s:v1.27.2-k3s1"
         "quay.io/open-cluster-management/registration-operator:v0.14.0"
-        "docker.io/bitnami/postgresql:16.0.0-debian-11-r13")
+        "quay.io/kubestellar/postgresql:16.0.0-debian-11-r13")
 
 for image in "${images[@]}"; do
     docker pull "$image" &
