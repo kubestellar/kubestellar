873e096bc6d15a86bd6fbc0f3f3c861f6a0f8e79
rxinui <rainui.ly@gmail.com>
2025-05-04 14:35:24 +0200

    fix: apply review fixes
    Signed-off-by: rxinui <rainui.ly@gmail.com>

--- hack/gha-reversemap.sh
+++ hack/gha-reversemap.sh
@@ -25,8 +25,6 @@ GITHUB_WORKFLOWS_PATH="./.github/workflows"
 REVERSEMAP_FILE=".gha-reversemap.yml"
 YQ_REQUIRED_VERSION="v4.45.1"
 GIT_COMMITSHA_LENGTH=40
-RE_ACTION_REF_AT="([a-zA-Z0-9 _/. \-]+)@"
-RE_ACTION_FULLREF="$RE_ACTION_REF_AT([a-zA-Z0-9 _/. \-]+)"
 
 ERR_YQ_DOWNLOAD_FAILED=50
 ERR_YQ_NOT_INSTALLED=60
@@ -38,19 +36,30 @@ help() {
 Harmonise '$GITHUB_WORKFLOWS_PATH' and the reversemap '$REVERSEMAP_FILE'
 
 Usage:
-    $0  <operation>
+    $0  <operation> [ARGUMENTS]
 
 Example:
     # Update actions/checkout to its latest release
     $0  update actions/checkout
 
 Operations:
-    apply                 -   Apply the commit sha of every actions listed in the reversemap as the version to use in all github workflows
-    update <action-ref>   -   Update the version of the given action reference to its latest release within the reverse map (sha, tag, urls)
-    sync                  -   Sync the reverse map values (sha, tag, urls) from the actions used in all github workflows
+    apply-reversemap        [...WORKFLOW_FILE]          -   Apply the commit sha of every actions listed in the reversemap 
+                                                            as the version to use in all github workflows
 
-Parameters:
-    <action-ref>:   {gh_owner}/{gh_repo}
+    update-action-version   ACTION_REF [...ACTION_REF]  -   Update the version of the given action reference within the reversemap
+                                                            (sha, tag, urls) to its latest regular release tag
+
+    update-reversemap       [...WORKFLOW_FILE]          -   Update the reverse map values (sha, tag, urls) from the 
+                                                            actions' version used in the github workflows
+
+Arguments:
+    apply-reversemap:
+        WORKFLOW_FILE                      -   Path of the workflow file. Must be *.yml
+    update-action-version:
+        ACTION_REF                         -   Expected to be formatted as "{gh_owner}/{gh_repo}"    
+    update-reversemap:
+        WORKFLOW_FILE                      -   Path of the workflow file. Must be *.yml
+
 EOF
 }
 
@@ -60,7 +69,7 @@ _loginfo() {
 }
 
 # Exit with an error
-_error_exit() {
+_exit_with_error() {
     echo -e "$(date -Iseconds);ERROR;$2" >&2
     exit "$1"
 }
@@ -75,10 +84,10 @@ _check_yq_version() {
     if command -v yq >/dev/null 2>&1; then
         INSTALLED_VERSION=$(yq --version 2>/dev/null)
         if ! [[ "$INSTALLED_VERSION" =~ $YQ_REQUIRED_VERSION ]]; then
-            _error_exit $ERR_YQ_NOT_INSTALLED "yq is installed but the version is $INSTALLED_VERSION. Required version is $YQ_REQUIRED_VERSION."
+            _exit_with_error $ERR_YQ_NOT_INSTALLED "yq is installed but the version is $INSTALLED_VERSION. Required version is $YQ_REQUIRED_VERSION."
         fi
     else
-        _error_exit $ERR_YQ_NOT_INSTALLED "yq is not installed."
+        _exit_with_error $ERR_YQ_NOT_INSTALLED "yq is not installed."
     fi
 }
 
@@ -94,7 +103,7 @@ _install_yq() {
     elif [[ "$arch" == "arm64" ]]; then
         arch="arm64"
     else
-        _error_exit $ERR_ARCH_UNSUPPORTED "Unsupported architecture: $arch"
+        _exit_with_error $ERR_ARCH_UNSUPPORTED "Unsupported architecture: $arch"
     fi
 
     yq_binary="yq_${os}_${arch}"
@@ -104,7 +113,7 @@ _install_yq() {
     curl -L -o "/tmp/${yq_binary}.tar.gz" "$yq_binary_url"
 
     if [[ $? -ne 0 ]]; then
-        _error_exit $ERR_YQ_DOWNLOAD_FAILED "Failed to download yq."
+        _exit_with_error $ERR_YQ_DOWNLOAD_FAILED "Failed to download yq."
     fi
 
     tar xzf "/tmp/${yq_binary}.tar.gz" -C /tmp
@@ -119,11 +128,12 @@ _fetch_sha_from_upstream_ref() {
     action_ref=$1
     tag_or_branch=$2
     action_ref_safe=$(echo "$action_ref" | cut -d '/' -f 1,2)
+    TMP_OUTPUT=/tmp/output.json
     API_GITHUB_BRANCH="https://api.github.com/repos/${action_ref_safe}/git/refs/heads/${tag_or_branch}"
     API_GITHUB_TAG="https://api.github.com/repos/${action_ref_safe}/git/refs/tags/${tag_or_branch}"
-    HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" -H "Authorization: Bearer $GITHUB_TOKEN" "$API_GITHUB_TAG")
+    HTTP_STATUS=$(curl -o "$TMP_OUTPUT" -s -w "%{http_code}" -H "Authorization: Bearer $GITHUB_TOKEN" "$API_GITHUB_TAG")
     if [[ $HTTP_STATUS -ge 200 && $HTTP_STATUS -lt 300 ]]; then
-        commit_sha=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" "$API_GITHUB_TAG" | jq -r '.object.sha')
+        commit_sha=$(jq -r '.object.sha' $TMP_OUTPUT)
         # _loginfo "tag api $commit_sha"
     else
         commit_sha=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" "$API_GITHUB_BRANCH" | jq -r '.object.sha')
@@ -144,8 +154,8 @@ _yq_update_reversemap() {
     yq ".${action_ref}.tag-url = \"https://github.com/${action_ref_safe}/tree/${action_tag}\"" -i $REVERSEMAP_FILE
 }
 
-# Synchronise reverse map values with an action full ref (ie. actions/checkout@v4.2.0)
-_sync_reversemap_with() {
+# Update the reverse map values with an action full ref (ie. actions/checkout@v4.2.0)
+_update_reversemap_with() {
     filename=$1
     for action_fullref in $(yq '.jobs[].steps[] | select(has("uses")) | .uses ' "$filename"); do
         action_ref=$(echo "$action_fullref" | cut -d '@' -f 1)
@@ -172,7 +182,13 @@ _update_action_version_infile() {
     file=$1
     action_ref=$2
     action_sha=$3
-    sed -E -i '' "s;$action_ref@.+;$action_ref@$action_sha;g" "$file"
+    sed_replace_expr="s;(uses:) $action_ref@[a-zA-Z0-9 \._\-]+;\1 $action_ref@$action_sha ;g"
+    if [[ "$(uname -s)" = "Darwin" ]]; then
+        # MacOS sed usage of -i option differs from Linux version.
+        sed -E -i '' "$sed_replace_expr" "$file"
+    else
+        sed -E -i "$sed_replace_expr" "$file"
+    fi
 }
 
 # Get commit sha of an action ref stored in the reverse map
@@ -180,39 +196,43 @@ _get_sha_from_reversemap() {
     action_ref=$1
     query=$(yq ".\"$action_ref\".sha" $REVERSEMAP_FILE)
     if [[ $? -ne 0 ]]; then
-        _error_exit "$query"
+        _exit_with_error "$query"
     fi
     _return "$query"
 }
 
-# Run operation apply
-run_apply() {
-    for file in "$GITHUB_WORKFLOWS_PATH"/*.yml; do
+# Apply the commit sha of every actions listed in the reversemap as the version to use in all github workflows
+run_apply_reversemap() {
+    files=$@
+    for file in $files; do
         _loginfo "applying '$REVERSEMAP_FILE' commit sha to be used in '$file' ..."
-        action_refs=$(grep -E -o "$RE_ACTION_REF_AT" "$file")
-        for action_ref in $action_refs; do
-            action_ref=${action_ref%?}
+        action_fullrefs=$(yq '.jobs[].steps[] | select(.uses) | .uses' "$file")
+        for action_fullref in $action_fullrefs; do
+            action_ref=$(echo "$action_fullref" | cut -d "@" -f 1)
             action_sha=$(_get_sha_from_reversemap "$action_ref")
-            _loginfo "found $action_ref in reversemap with sha=$action_sha"
+            _loginfo "found '$action_ref' in reversemap with sha=$action_sha"
             _update_action_version_infile "$file" "$action_ref" "$action_sha"
         done
     done
 }
 
-# Run operation update
-run_update() {
-    dependency_arg=$1
-    _loginfo "updating dependency '$dependency_arg' tag to latest version available inside reverse map '$REVERSEMAP_FILE'"
-    latest_tag=$(_fetch_latest_tag "$dependency_arg")
-    action_sha=$(_fetch_sha_from_upstream_ref "$dependency_arg" "$latest_tag")
-    _yq_update_reversemap "$dependency_arg" "$latest_tag" "$action_sha"
+# Update the version of the given action references within the reverse map (sha, tag, urls) to its latest regular release tag
+run_update_action_version() {
+    action_refs=$@
+    for action_ref in $action_refs; do
+        _loginfo "updating dependency '$action_ref' tag to latest version available inside reverse map '$REVERSEMAP_FILE'"
+        latest_tag=$(_fetch_latest_tag "$action_ref")
+        action_sha=$(_fetch_sha_from_upstream_ref "$action_ref" "$latest_tag")
+        _yq_update_reversemap "$action_ref" "$latest_tag" "$action_sha"
+    done
 }
 
-# Run operation sync
-run_sync() {
-    for file in "$GITHUB_WORKFLOWS_PATH"/*.yml; do
-        _loginfo "syncing $REVERSEMAP_FILE with actions from '$file' ..."
-        _sync_reversemap_with "$file"
+# Update the reverse map values (sha, tag, urls) from the actions used in given github workflows
+run_update_reversemap() {
+    files=$@
+    for file in $files; do
+        _loginfo "updating $REVERSEMAP_FILE with actions from '$file' ..."
+        _update_reversemap_with "$file"
     done
 }
 
@@ -220,25 +240,36 @@ run_sync() {
 run_cli() {
     operation_arg=$1
     case $operation_arg in
-    "apply")
-        _loginfo "running $1"
-        run_apply
-        ;;
-    "update")
-        dependency_arg=$2
-        if [[ -z $dependency_arg ]]; then
-            _error_exit -1 "missing dependency name to update."
+    "apply-reversemap")
+        shift
+        files=$@
+        if [[ "$#" -eq 0 ]]; then
+            files="$GITHUB_WORKFLOWS_PATH/*.yml"
         fi
-        _loginfo "running $1"
-        run_update "$dependency_arg"
+        _loginfo "running $operation_arg on $files"
+        run_apply_reversemap $files
         ;;
-    "sync")
-        _loginfo "running $1"
-        run_sync
+    "update-action-version")
+        shift
+        action_refs=$@
+        if [[ "$#" -eq 0 ]]; then
+            _exit_with_error 1 "missing action ref to update. Format must be '{gh_owner}/{gh_repo}'"
+        fi
+        _loginfo "running $operation_arg on $action_refs"
+        run_update_action_version $action_refs
+        ;;
+    "update-reversemap")
+        shift
+        files=$@
+        if [[ "$#" -eq 0 ]]; then
+            files="$GITHUB_WORKFLOWS_PATH/*.yml"
+        fi
+        _loginfo "running $operation_arg on $files"
+        run_update_reversemap $files
         ;;
     *)
         help
-        exit 2
+        exit 1
         ;;
     esac
 
@@ -247,14 +278,13 @@ run_cli() {
 # Execute the main program
 main() {
     if [[ -z $GITHUB_TOKEN ]]; then
-        _error_exit $ERR_GITHUB_TOKEN_INVALID "no github pat"
+        _exit_with_error $ERR_GITHUB_TOKEN_INVALID "environment variable GITHUB_TOKEN is not set."
     fi
     _check_yq_version
     if [[ $? -eq $ERR_YQ_NOT_INSTALLED ]]; then
         _install_yq
     fi
     run_cli $@
-
 }
 
 main $@
