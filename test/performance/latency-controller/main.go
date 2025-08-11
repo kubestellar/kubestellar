7df03a5c063f1fbd23e36d82b0b57d3c3ba7ad42
asmit27rai <raiasmit10@gmail.com>
2025-08-12 02:06:30 +0530

    Fix the tests
    Signed-off-by: asmit27rai <raiasmit10@gmail.com>

--- test/performance/latency-controller/main.go
+++ test/performance/latency-controller/main.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2023 The KubeStellar Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package main
 
 import (
@@ -107,13 +123,13 @@ func main() {
 	// Automatic detection: use explicit path > in-cluster > default kubeconfig
 	mgrCfg := resolveConfig(wdsKubeconfigPath, kubeconfigPath, wdsContext)
 	mgr, err := ctrl.NewManager(mgrCfg, ctrl.Options{
-		Scheme: scheme,
-		Metrics: metricsserver.Options{BindAddress: metricsAddr, SecureServing: secureMetrics},
+		Scheme:                 scheme,
+		Metrics:                metricsserver.Options{BindAddress: metricsAddr, SecureServing: secureMetrics},
 		WebhookServer:          webhook.NewServer(webhook.Options{}),
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "34020a28.kubestellar.io",
-		Client: client.Options{Cache: &client.CacheOptions{Unstructured: true}},
+		Client:                 client.Options{Cache: &client.CacheOptions{Unstructured: true}},
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
