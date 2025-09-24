3a50202fb13cb6bad69d5c008d77ecdda444727b
Naman Jain <namanjain9271@gmail.com>
2025-09-24 20:08:24 +0530

    fixup (#79)

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: isProd ? "/docs" : "",
-  assetPrefix: isProd ? "/docs/" : "",
+  output: "standalone",
   images: {
     unoptimized: true,
   },
+  experimental: {
+    optimizePackageImports: ['@/components']
+  }
 };
 
 export default nextConfig;
