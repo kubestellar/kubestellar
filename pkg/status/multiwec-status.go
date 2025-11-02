69199deaf500d600d7f338a94a3fb34a839951f1
Gaurab Khanal <khanalgaurab98@gmail.com>
2025-11-02 22:25:23 +0545

    fix: fix lint err
    Signed-off-by: Gaurab Khanal <khanalgaurab98@gmail.com>

--- pkg/status/multiwec-status.go
+++ pkg/status/multiwec-status.go
@@ -1,5 +1,5 @@
 /*
-Copyright YEAR The KubeStellar Authors.
+Copyright 2025 The KubeStellar Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@ package status
 
 import (
 	"context"
+
 	"k8s.io/klog/v2"
 
 	"github.com/kubestellar/kubestellar/pkg/util"
