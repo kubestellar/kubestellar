0018d9fe5b13c975ff450039f64d753d484863ed
Naman Jain <namanjain9271@gmail.com>
2025-12-05 13:28:48 +0530

    fixing links (#340)
    * fixing links
    
    * fix formatting

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,95 @@ const nextConfig: NextConfig = {
     optimizePackageImports: ["@/components"],
   },
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  async redirects() {
+    return [
+      {
+        source: "/agenda",
+        destination:
+          "https://docs.google.com/document/d/1XppfxSOD7AOX1lVVVIPWjpFkrxakfBfVzcybRg17-PM/edit?usp=share_link",
+        permanent: true,
+      },
+      {
+        source: "/blog",
+        destination:
+          "https://medium.com/@kubestellar/list/predefined:e785a0675051:READING_LIST",
+        permanent: true,
+      },
+      {
+        source: "/code",
+        destination: "https://github.com/kubestellar/kubestellar",
+        permanent: true,
+      },
+      {
+        source: "/community",
+        destination: "https://docs.kubestellar.io/stable/Community/_index/",
+        permanent: true,
+      },
+      {
+        source: "/drive",
+        destination:
+          "https://drive.google.com/drive/u/1/folders/1p68MwkX0sYdTvtup0DcnAEsnXElobFLS",
+        permanent: true,
+      },
+      {
+        source: "/infomercial",
+        destination: "https://youtu.be/rCjQAdwvZjk",
+        permanent: true,
+      },
+      {
+        source: "/join_us",
+        destination: "https://groups.google.com/g/kubestellar-dev",
+        permanent: true,
+      },
+      {
+        source: "/joinus",
+        destination: "https://groups.google.com/g/kubestellar-dev",
+        permanent: true,
+      },
+      {
+        source: "/ladder",
+        destination: "https://kubestellar.io/en/ladder",
+        permanent: true,
+      },
+      {
+        source: "/ladder_stats",
+        destination:
+          "https://docs.google.com/spreadsheets/d/16CxUk2tNbTB-Si0qRVwIrI_f19t9HwSby9C1djMN7Sc/edit?usp=sharing",
+        permanent: true,
+      },
+      {
+        source: "/linkedin",
+        destination:
+          "https://www.linkedin.com/feed/hashtag/?keywords=kubestellar",
+        permanent: true,
+      },
+      {
+        source: "/quickstart",
+        destination: "https://kubestellar.io/en/quick-installation",
+        permanent: true,
+      },
+      {
+        source: "/slack",
+        destination: "https://cloud-native.slack.com/archives/C097094RZ3M",
+        permanent: true,
+      },
+      {
+        source: "/survey",
+        destination: "https://forms.gle/WJ7N6ZVtp44D9NK79",
+        permanent: true,
+      },
+      {
+        source: "/tv",
+        destination: "https://youtube.com/@kubestellar",
+        permanent: true,
+      },
+      {
+        source: "/youtube",
+        destination: "https://youtube.com/@kubestellar",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 const configWithNextra = withNextra(nextConfig);
