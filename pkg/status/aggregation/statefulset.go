af7bb138ff162eda54a82194a35c93502274c599
Rishi Jat <rishijat098@gmail.com>
2026-01-09 01:16:26 +0530

    fix boilerplate
    Signed-off-by: Rishi Jat <rishijat098@gmail.com>

--- pkg/status/aggregation/statefulset.go
+++ pkg/status/aggregation/statefulset.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-	http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package aggregation
 
 func AggregateStatefulSetStatus(statuses []map[string]any) (map[string]any, error) {
@@ -23,6 +24,7 @@ func AggregateStatefulSetStatus(statuses []map[string]any) (map[string]any, erro
 	aggregatedStatus["readyReplicas"] = GetMin(statuses, "readyReplicas")
 	aggregatedStatus["currentReplicas"] = GetMin(statuses, "currentReplicas")
 	aggregatedStatus["updatedReplicas"] = GetMin(statuses, "updatedReplicas")
+	aggregatedStatus["observedGeneration"] = GetMin(statuses, "observedGeneration")
 
 	// Aggregate conditions using the generic condition aggregation rules
 	aggregatedStatus["conditions"] = AggregateConditions(statuses)
