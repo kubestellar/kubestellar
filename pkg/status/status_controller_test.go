285738d0c7cedcd307cedbd907a17cf4bf20ca18
Norway-02 <anshulkhetade02@gmail.com>
2026-07-12 22:38:00 +0530

    fix(status): fix loopclosure vet warning in tests
    Signed-off-by: Norway-02 <anshulkhetade02@gmail.com>

--- pkg/status/status_controller_test.go
+++ pkg/status/status_controller_test.go
@@ -40,6 +40,7 @@ func TestObjNotInThisWDS(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			obj := makeObj(tt.labels)
 			result := objNotInThisWDS(obj, tt.thisWDS)
